ce73dce6fdf5dbd4a830a87cb71f3805fe623f2b
老李拉面馆 <narisdrum@outlook.com>
2016-11-06 20:51:13 +0800

    firstDemo
    初代版本，现在只显示HQ链接，啥时候有时间再写吧。。
    
    说不定以后会加上
    SQ链接
    跟搜索20条记录

---
+++ UWP_TestAppUnit1106/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using static UWP_TestAppUnit1106.Music0;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_TestAppUnit1106
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //定义泛型集合（音乐信息）
+        List<music> obj;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        //获取音乐信息
+        private async void Get_Music(object sender, RoutedEventArgs e)
+        {
+            var apiUrl_GetJson = string.Format("http://api.itwusun.com/music/search/qq/1?format=json&sign=a5cc0a8797539d3a1a4f7aeca5b695b9&keyword={0}", this.tbox_musicName.Text);
+            //开始准备访问api服务器 实例化client
+            HttpClient client = new HttpClient();
+            //把自己本地整理好的数据发送给api服务器
+            var json = await client.GetStringAsync(new Uri(apiUrl_GetJson));
+            //判断是否有json返回
+            if (json == null)
+            {
+                return;
+            }
+            obj = FromJsonTo<List<music>>(json);//耳朵提供的方法
+            tb_Paste();
+        }
+        public void tb_Paste()
+        {
+            tb_songName.Text = obj[0].SongName;
+            tb_songArtist.Text = obj[0].Artist;
+            MusicUrl.Text = obj[0].HqUrl;
+        }
+        //Json转泛型 方法
+        public static T FromJsonTo<T>(string jsonString)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+            T jsonObject = (T)ser.ReadObject(ms);
+            ms.Dispose();
+            return jsonObject;
+        }
+
+        
+    }
+
+    
+
+}
---
+++ UWP_TestAppUnit1106/Music0.cs
@@ -0,0 +1,42 @@
+﻿namespace UWP_TestAppUnit1106
+{
+    public class Music0
+    {
+        public class music 
+        {
+            public string SongId { get; set; }
+            public string SongName { get; set; }
+            public string SubTitle { get; set; }
+            public string Artist { get; set; }
+            public string ArtistSubTitle { get; set; }
+            public string AlbumId { get; set; }
+            public string Album { get; set; }
+            public string AlbumSubTitle { get; set; }
+            public string AlbumArtist { get; set; }
+            public string CollectName { get; set; }
+            public string Length { get; set; }
+            public string Size { get; set; }
+            public string BitRate { get; set; }
+            public string FlacUrl { get; set; }
+            public string AacUrl { get; set; }
+            public string SqUrl { get; set; }
+            public string HqUrl { get; set; }
+            public string LqUrl { get; set; }
+            public string ListenUrl { get; set; }
+            public string CopyUrl { get; set; }
+            public string PicUrl { get; set; }
+            public string LrcUrl { get; set; }
+            public string KlokLrc { get; set; }
+            public string MvId { get; set; }
+            public string MvUrl { get; set; }
+            public string VideoUrl { get; set; }
+            public string Language { get; set; }
+            public string Company { get; set; }
+            public string Year { get; set; }
+            public string Disc { get; set; }
+            public string TrackNum { get; set; }
+            public string Type { get; set; }
+        }
+
+    }
+}
